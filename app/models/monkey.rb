b4f1b884ef067597c59cb38ff1a3261e064f1921
nborbe <nborbe@kaufmanrossin.com>
2020-02-20 10:56:07 -0500

    update monkey model for friendship relationship

--- app/models/monkey.rb
+++ app/models/monkey.rb
@@ -2,7 +2,7 @@ class Monkey < ApplicationRecord
 
     has_many :friendships, dependent: :destroy
 
-    has_many :friends, through: :friendships
+    has_many :friends, through: :friendshipssudo
 
 
 
